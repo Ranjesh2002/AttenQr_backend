9c38b054df409e74a7dfaa18ea161bae8641fc6a
ranjesh <rs0036870@gmail.com>
2025-06-09 21:53:49 +0545

    made changes to setting

--- AttenQR_project/settings.py
+++ AttenQR_project/settings.py
@@ -72,6 +72,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.expo\.dev$",               # For Expo Go mobile app (if using)
+]
+
 ROOT_URLCONF = 'AttenQR_project.urls'
 
 TEMPLATES = [
