ed056e4c98821ba75f3b2abcce23e4b82ef32446
ranjesh <rs0036870@gmail.com>
2025-06-19 21:56:13 +0545

    created views for student attendance

--- Atten_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ClassSession
-
-class ClassSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ClassSession
-        fields = '__all__'
